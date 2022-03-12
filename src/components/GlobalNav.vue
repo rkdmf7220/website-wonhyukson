8700ed4facdc22f0e39537ed34781e3468a4ab1c
LSM <lsm4551@on.mjc.ac.kr>
2022-03-12 20:29:19 +0900

    reset 추가 및 GNB 스타일링

--- src/components/GlobalNav.vue
+++ src/components/GlobalNav.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav">
-    <router-link to="/">주소</router-link>
+    <router-link to="/" class="logo">Wonhyuk Son</router-link>
     <ul>
       <li v-for="item in menu" :key="item.name">
         <router-link :to="item.path">{{item.name}}</router-link>
@@ -30,6 +30,32 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  #nav{
+    width: 240px;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding: 100px 0 0 32px;
+    box-sizing: border-box;
 
+    & .logo{
+      font-weight: 700;
+      font-size: 1.25rem;
+      padding: 16px;
+      display: inline-block;
+    }
+
+    & ul{
+      font-size: 1rem;
+      margin-top: 16px;
+
+      & a{
+        padding-left: 16px;
+        line-height: 2rem;
+        display: block;
+      }
+    }
+  }
 </style>
