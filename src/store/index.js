bfc793a7e43718b29afd775bb325b849c8ba4c7a
LSM <lsm4551@on.mjc.ac.kr>
2022-03-20 16:22:44 +0900

    router 구조 마무리

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,38 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
+import axios from "axios";
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        worksList: []
+    },
+    mutations: {
+        worksList(state, list) {
+            state.worksList = list;
+        }
+    },
+    actions: {
+        // updateWorksList({commit}, payload) {
+        //   // console.log('commit은', commit)
+        //   console.log('payload는', payload)
+        //   commit('worksList', payload.list)
+        // },
+        loadWorksList({commit}) {
+            axios.get("../mockup/data-result.json")
+                .then(response => {
+                    console.log('response 내용은', response);
+                    if (response.statusText === "OK") {
+                        // this.listData = response.data;
+                        // this.$store.dispatch('updateWorksList', {list: response.data})
+
+                        commit('worksList', response.data)
+                    } else {
+                        // TODO: error handling.
+                    }
+                }).catch(error => {
+                console.log('error', error);
+                // TODO: error handling.
+            });
+        }
+    },
+    modules: {}
 })
