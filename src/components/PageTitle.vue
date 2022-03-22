29142d52c37f1dbef20426660ec2995969755098
LSM <lsm4551@on.mjc.ac.kr>
2022-03-22 13:58:48 +0900

    page title 컴포넌트화

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="header">
+    <h1>{{$route.name}}</h1>
+    <div class="lang-container">
+      <ul>
+        <li @click="onClickLang(Constants.lang.kr)"><span>kr</span></li>
+        <li class="active" @click="onClickLang(Constants.lang.en)"><span>en</span></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import Constants from "../contants/constants";
+export default {
+  name: "PageTitle",
+  data() {
+    return{
+      Constants
+    }
+  },
+  methods: {
+    onClickLang(lang) {
+      this.$store.dispatch('setLang', lang)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .header{
+    display: flex;
+
+    .lang-container{
+      margin-left: auto;
+
+      ul{
+        display: flex;
+
+        li{
+          color: #999;
+          cursor: pointer;
+
+          &.active{
+            color: #000;
+          }
+        }
+      }
+    }
+  }
+</style>
