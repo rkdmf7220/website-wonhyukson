49cef8af6ae005018cc87ea2856818c871c71e24
rkdmf7220 <rkdmf7220@gmail.com>
2022-03-26 00:02:09 +0900

    archive 페이지 수정

--- src/views/Archive.vue
+++ src/views/Archive.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="wrap">
     <page-title/>
+    <gallery-list/>
   </div>
 </template>
 
 <script>
 import PageTitle from "../components/PageTitle";
+import GalleryList from "../components/GalleryList";
 export default {
   name: "Archive",
-  components: {PageTitle}
+  components: {GalleryList, PageTitle}
 }
 </script>
 
