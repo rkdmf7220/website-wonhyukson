aa9bae5755c6ba6ca30c9051f4b0982e0d947339
2sangmin <rkdmf7220@gmail.com>
2025-02-25 21:47:22 +0900

    분석 플러그인 비활성화

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  productionSourceMap: false,
+};
+
+/*
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+
+    configureWebpack: {
+        plugins: [new BundleAnalyzerPlugin()]
+    }
+
+};*/
