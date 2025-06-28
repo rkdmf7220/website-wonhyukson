149053ed6088c5a77e52915b29e718b047f010b8
2sangmin <lsm4551@on.mjc.ac.kr>
2025-06-28 20:21:59 +0900

    import 주소 변경

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap');
-@import "src/assets/css/reset.css";
+@import "/src/assets/css/reset.css";
 
 body{
   font-size: 16px;
