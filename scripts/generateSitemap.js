5c68170a635838d86331a257afaec0024a1d3d7a
2sangmin <lsm4551@on.mjc.ac.kr>
2025-10-22 21:04:32 +0900

    파일 생성 테스트

---
+++ scripts/generateSitemap.js
@@ -0,0 +1,29 @@
+import {writeFileSync, readFileSync} from 'fs';
+import {resolve} from 'path';
+
+// file path
+const projectRoot = process.cwd();
+const xmlFilePath = resolve(projectRoot, 'dist', 'sitemap.xml');
+const jsonFilePath = (lang) => resolve(projectRoot, 'public/sample', lang, 'data-result.json');
+
+
+
+// test
+// const jsonData = readFileSync(jsonFilePath('ko', 'cv'), 'utf8');
+const koJsonData = readFileSync(jsonFilePath('ko'), 'utf8');
+const enJsonData = readFileSync(jsonFilePath('en'), 'utf8');
+
+// todo: static - intro, main, sculpture, painting, typography, texts, archives, cv, news
+// todo: dynamic - sculpture, sculpture, painting, typography, archives, news
+
+const data = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <>${jsonData}</>
+</urlset>`;
+
+
+
+
+
+// write file
+writeFileSync(xmlFilePath, data, 'utf8');
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path';
+import { exec } from 'child_process';
+
+// 커맨드라인 인자에서 --genKey 옵션이 있는지 확인
+const shouldGenerateSitemap = process.argv.includes('--genSitemap');
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), {
+    name: 'generate-sitemap-in-build',
+    apply: 'build', // 빌드 시에만 작동
+    closeBundle() {
+      if (!shouldGenerateSitemap) return;
+      console.log('[vite-plugin] --genKey detected. Running generateSitemap.js...');
+      const scriptPath = path.resolve(__dirname, 'scripts/generateSitemap.js');
+      exec(`node "${scriptPath}"`, (err, stdout, stderr) => {
+        if (err) {
+          console.error('[vite-plugin] Error running generateSitemap.js:', err);
+          return;
+        }
+        if (stderr) {
+          console.error('[vite-plugin] generateSitemap.js stderr:', stderr);
+        }
+        console.log(stdout);
+      });
+    },
+  }],
 })
